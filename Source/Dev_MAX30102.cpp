129c0089d3f3c123f814c4c06345157525c0a492
chenmtech <1276094137@qq.com>
2020-12-10 13:08:47 +0800

    用17bit分辨率比较好；

--- Source/Dev_MAX30102.cpp
+++ Source/Dev_MAX30102.cpp
@@ -193,9 +193,9 @@ extern void MAX30102_Setup(uint8 mode, uint16 sampleRate)
     activeLED = 2;
   }
   
-  setFIFOAverage(MAX30102_SAMPLEAVG_8);
+  setFIFOAverage(MAX30102_SAMPLEAVG_8); // 8 samples averaging
   
-  setADCRange(MAX30102_ADCRANGE_4096);
+  setADCRange(MAX30102_ADCRANGE_2048); // 
   
   if (sampleRate < 100) setSampleRate(MAX30102_SAMPLERATE_50); //Take 50 samples per second
   else if (sampleRate < 200) setSampleRate(MAX30102_SAMPLERATE_100);
@@ -207,7 +207,7 @@ extern void MAX30102_Setup(uint8 mode, uint16 sampleRate)
   else if (sampleRate == 3200) setSampleRate(MAX30102_SAMPLERATE_3200);
   else setSampleRate(MAX30102_SAMPLERATE_50);
   
-  setPulseWidth(MAX30102_PULSEWIDTH_215); //17 bit resolution
+  setPulseWidth(MAX30102_PULSEWIDTH_215); //118: 16bit 215:17bit 411:18bit resolution
   
   setPulseAmplitudeRed(0x1F); // 0x0F : 3.0mA, 0x1F: 6.2mA
   setPulseAmplitudeIR(0x1F);
@@ -508,11 +508,11 @@ static void readOneSampleData()
   uint8 buff[6] = {0};
   readMultipleBytes(MAX30102_FIFODATA, activeLED*3, buff);
   uint32 num32 = BUILD_UINT32(buff[2], buff[1], buff[0], 0x00);
-  uint16 red = (uint16)(num32>>2);
+  uint16 red = (uint16)(num32>>1);
   uint16 ir = 0;
   if(activeLED > 1) {
     num32 = BUILD_UINT32(buff[5], buff[4], buff[3], 0x00);
-    ir = (uint16)(num32>>2);
+    ir = (uint16)(num32>>1);
   }
   pfnMAXDataCB(red, ir, activeLED);
 }
